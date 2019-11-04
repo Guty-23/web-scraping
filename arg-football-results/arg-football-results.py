04e479fa4ce81d2a4ec3742ba7c15f1bd01beab8
Facundo <facumgutierrez@gmail.com>
2019-11-03 23:05:25 -0300

    Start working on data_sheet

--- arg-football-results/arg-football-results.py
+++ arg-football-results/arg-football-results.py
@@ -1,5 +1,11 @@
 # Usa python3
-import requests,datetime
+import requests,datetime,openpyxl
+
+from openpyxl import Workbook
+from openpyxl.styles import Font, Style, PatternFill, Border, Side, Alignment, Protection
+from openpyxl.chart.layout import Layout, ManualLayout
+from openpyxl.chart import BarChart, Series, Reference
+
 from bs4 import BeautifulSoup
 
 from match import Match
@@ -9,31 +15,41 @@ from match import Match
 ################################
 
 years = [2018,2019]
-teams_each_year = {2018:26,2019:24}
-start_each_year = {2018:1342,2019:2006}
-finish_each_year = {2018:-195,2019:-135}
-rounds_each_year = {2018:25,2019:13}
+teams_year = {2018:26,2019:24}
+start_year = {2018:1342,2019:2006}
+finish_year = {2018:-195,2019:-135}
+rounds_year = {2018:25,2019:13}
 wikipedia_fields = 6
 
-year_each_month = {year:{'january':year+1,'enero':year+1, 'february':year+1,'febrero':year+1, 
+year_month = {year:{'january':year+1,'enero':year+1, 'february':year+1,'febrero':year+1, 
 						  'march':year+1,'marzo':year+1, 'april':year+1,'abril':year+1,	'may':year+1,'mayo':year+1,
 						  'june':year+1,'junio':year+1, 'july':year,'julio':year, 'august':year,'agosto':year,
 						  'september':year,'septiembre':year,'setiembre':year, 'october':year,'octubre':year, 
 						   'november':year,'noviembre':year, 'december':year,'diciembre':year} for year in years}
 
-index_each_month = {year:{'january':1,'enero':1, 'february':2,'febrero':2, 'march':3,'marzo':3, 'april':4,'abril':4,
+index_month = {year:{'january':1,'enero':1, 'february':2,'febrero':2, 'march':3,'marzo':3, 'april':4,'abril':4,
 					'may':5,'mayo':5,'june':6,'junio':6, 'july':7,'julio':7, 'august':8,'agosto':8,
 					'september':9,'septiembre':9,'setiembre':9, 'october':10,'octubre':10, 
 					'november':11,'noviembre':11, 'december':12,'diciembre':12} for year in years}
 					
 index_datetime_day = {0:'monday', 1:'tuesday', 2:'wednesday', 3:'thursday', 4:'friday', 5:'saturday', 6:'sunday'}
 
+def get_data_from_year(year):
+	wiki = "https://es.wikipedia.org/wiki/Campeonato_de_Primera_División_" + str(year) +"-" + str(year+1)[2:] + "_(Argentina)"
+	file_name = 'horarios_superliga_' + str(year) + '-' + str(year+1)[2:] + '.xlsx'
+	website_url = requests.get(wiki).text
+	soup = BeautifulSoup(website_url,'lxml')
+	teams = teams_year[year]
+	rounds = rounds_year[year]
+	rows = (teams//2)*rounds
+	return (soup,teams,rounds,rows,file_name)
+
 def get_matches_from_wikipedia(soup,teams,rounds,rows,year):
 
-    matches_raw = [['' for x in range(wikipedia_fields)] for row in range(rows)]
+    matches_raw = [['' for x in range(wikipedia_fields)] + [row//(teams//2)+1] for row in range(rows)]
     i = 0
-    start = start_each_year[year]   # Search on the table watching HTML from wikipedia, aided by bs4
-    finish = finish_each_year[year] # Search on the table watching HTML from wikipedia, aided by bs4
+    start = start_year[year]   # Search on the table watching HTML from wikipedia, aided by bs4
+    finish = finish_year[year] # Search on the table watching HTML from wikipedia, aided by bs4
     
     for x in soup.select('table tbody tr td')[start:finish]:
         while(i < wikipedia_fields*rows and matches_raw[i//wikipedia_fields][i%wikipedia_fields] != ''):
@@ -44,7 +60,7 @@ def get_matches_from_wikipedia(soup,teams,rounds,rows,year):
                 field = "Entry without value"
             if i % wikipedia_fields == 4:  # Date
                 day_month = list(map(lambda s : s.strip().lower(),field.split(' de ')))
-                field = datetime.date(year_each_month[year][day_month[1]], index_each_month[year][day_month[1]], int(day_month[0]))
+                field = datetime.date(year_month[year][day_month[1]], index_month[year][day_month[1]], int(day_month[0]))
             k = 1
             if x.get('rowspan') != None:
                 k = int(x.get('rowspan'))
@@ -54,37 +70,48 @@ def get_matches_from_wikipedia(soup,teams,rounds,rows,year):
     match_list = []
     for match in matches_raw:
         hours, minutes = list(map(int, match[5].split(':')))
-        home_goals,away_goals = match[1].split('-')
+        home_goals,away_goals = map(lambda s : s.strip(), match[1].split('-'))
         home,away = match[0],match[2]
         stadium = match[3]
         date = datetime.datetime(match[4].year, match[4].month, match[4].day, hours, minutes)
-        match_list.append(Match(date,stadium,home,away,home_goals,away_goals))
+        round_slot = match[6]
+        match_list.append(Match(date,round_slot,stadium,home,away,home_goals,away_goals,False))
     
     return match_list
+    
+def generate_data_sheet(file_name,match_list):
+	wb = openpyxl.Workbook()
+
+	sheet = wb.get_active_sheet()
+	sheet.title = 'Horarios Superliga'
+
+	sheet_columns = ["Fecha","Dia Calendario","Dia Semana","Horario","Local","Visitante"]
+	sheet.freeze_panes = 'A2'
+	
+	
+	
+	wb.save(file_name)
        
 def main():
-
 	for year in years:
-		print("\n\n\n#######################\n# Superliga " + str(year) + "-" + str(year+1) + " #\n#######################\n")
-		wiki = "https://es.wikipedia.org/wiki/Campeonato_de_Primera_División_" + str(year) +"-" + str(year+1)[2:] + "_(Argentina)"
-		website_url = requests.get(wiki).text
-		soup = BeautifulSoup(website_url,'lxml')
-
-		teams = teams_each_year[year]
-		rounds = rounds_each_year[year]
-		rows = (teams//2)*rounds
-		
+		soup,teams,rounds,rows,file_name = get_data_from_year(year)
 		match_list = sorted(get_matches_from_wikipedia(soup,teams,rounds,rows,year))
-		for m in match_list:
-			print(m)
+		generate_data_sheet(file_name,match_list)
+		
+			
+		
+		
+		
 			
 
 if __name__ == '__main__':
 	main()
+	
+	
     
         
     # TO DO: Hacer mejor con pandas.
-
+	#~ print("\n\n\n#######################\n# Superliga " + str(year) + "-" + str(year+1) + " #\n#######################\n")
     
     # nombres_equipos = equipos = sorted(list(set([p[0] for p in partidos])))
     # for eq in nombres_equipos:
--- arg-football-results/match.py
+++ arg-football-results/match.py
@@ -1,21 +1,31 @@
 # Usa python3
 
 class Match():
-	def __init__(self, date, stadium, home, away, home_goals, away_goals):
+	def __init__(self, date, round_slot, stadium, home, away, home_goals, away_goals, domestic):
 		# Everything is a string, except for the date
 		self.date = date
+		self.round_slot = round_slot
 		self.stadium = stadium
 		self.home = home
 		self.away = away
 		self.home_goals = home_goals
 		self.away_goals = away_goals
+		self.domestic = domestic
+		
+	def get_home_goals(self):
+		return int(self.home_goals)
+		
+	def get_away_goals(self):
+		return int(self.away_goals)
 		
 	def __str__(self):
-		return ",".join([self.home,
+		return ",".join([str(self.round_slot),
+						 self.home,
 						 self.away,
 						 self.home_goals + "-" + self.away_goals,
 						 self.stadium,
-						 str(self.date)])
+						 str(self.date),
+						 str(self.domestic)])
 	
 	def __lt__ (self,other):
 		if self.date == other.date:
